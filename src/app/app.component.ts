d6b108a942a85adfb4a70027b6cbaae1c17b8247
thyago00 <thyagoro6@gmail.com>
2021-08-01 20:25:24 -0300

    calendario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { buildEntryKey, CalendarOptions } from '@fullcalendar/angular'; // useful for typechecking
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calendario';
+  
+  calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth',
+    customButtons: {
+      feriados:{
+        text: 'Feriados',
+        click: function(){
+         this.toggleWeekends();
+        }
+      }
+   
+    },
+    dateClick: this.handleDateClick.bind(this), // bind is important!
+    events: [
+      { title: 'event 1', start: '2021-07-30T14:30:00', end: '2021-07-30T18:30:00', display: 'background: ' },
+      { title: 'event 2', date: '2021-07-31' }
+    ],
+    // weekends: false, // initial value,
+    locale: 'pt-br',
+    headerToolbar: {
+      start: 'prev,next today feriados',
+      center: 'title',
+      end: '',
+    },
+    droppable: true,
+    buttonText:{
+      today: 'Hoje'
+    },
+    selectable: true,
+    nowIndicator: true,
+    // fixedWeekCount: true,
+    contentHeight: 600,
+		height: 600,
+
+  };
+
+  handleDateClick(arg) {
+    alert('date click! ' + arg.dateStr)
+  }
+
+  toggleWeekends() {
+    this.calendarOptions.weekends = !this.calendarOptions.weekends // toggle the boolean!
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FullCalendarModule } from '@fullcalendar/angular'; // must go before plugins
+import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
+import interactionPlugin from '@fullcalendar/interaction'; // a plugin!
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventosComponent } from './eventos/eventos.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+
+FullCalendarModule.registerPlugins([ // register FullCalendar plugins
+  dayGridPlugin,
+  interactionPlugin
+]);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FullCalendarModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
